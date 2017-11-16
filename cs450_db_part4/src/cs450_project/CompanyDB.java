07c02bfb477a8ab8347d193e17ba85927387f75d
Ivan Diaz <idiaz3@gmu.edu>
2017-11-16 01:04:26 -0500

    Added a new method to grab list of Projects

--- cs450_db_part4/src/cs450_project/CompanyDB.java
+++ cs450_db_part4/src/cs450_project/CompanyDB.java
@@ -4,6 +4,7 @@
 package cs450_project;
 import java.sql.*;
 import java.io.*;
+import java.util.*;
 
 /**
  * @author ivan
@@ -32,6 +33,12 @@ public class CompanyDB {
 		
 	}
 	
+	/**
+	 * Boolean method to check if the SSN corresponds to a manager in the database
+	 * @param ssn
+	 * @return true if it is, false if not
+	 * @throws SQLException
+	 */
 	public boolean isManager(String ssn) throws SQLException {
 		String query = "select * from department where mgrssn = ?";
 		String mgrSsn = null;
@@ -51,6 +58,24 @@ public class CompanyDB {
 		return false;
 	}
 
-	
+	public ArrayList projectList() throws SQLException {
+		ArrayList<String> listOfProjects = new ArrayList<String>();
+		String query = "select pname from project";
+		String project = "";
+		p = conn.prepareStatement(query);
+		p.clearParameters();
+		r = p.executeQuery();
+		while(r.next()) {
+			project = r.getString(1);
+			listOfProjects.add(project);
+		}
+		/* Print out the list of projects to make sure things are right */
+		Iterator itr = listOfProjects.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+		return listOfProjects;
+
+	}
 
 }
