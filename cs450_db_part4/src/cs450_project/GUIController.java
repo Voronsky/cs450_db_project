0a055549b8c5b64b56fb8396efd65e41e2958cdb
Ivan Diaz <idiaz3@gmu.edu>
2017-11-16 15:11:44 -0500

    Fixed new window not opening

--- cs450_db_part4/src/cs450_project/GUIController.java
+++ cs450_db_part4/src/cs450_project/GUIController.java
@@ -23,60 +23,74 @@ public class GUIController {
 	private TextField ssn_input;
 	@FXML
 	Button login_btn;
+	@FXML
+	Button add_emp_btn;
 	private String ssn;
 	private boolean mgrCheck;
-	Parent root;
 	
 	@FXML
 	protected void initialize() {
+		/*login_btn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event)  {
+				getSSNInput();
+				System.out.println("-----INFO: Launching MgrScreen ----");
+				launchManagerScreen(event);
+				
+			}
+		});*/
+		
 		login_btn.setOnAction(new EventHandler<ActionEvent>() {
+			Boolean result;
 			@Override
 			public void handle(ActionEvent event) {
+				Parent root;
 				try {
-					getSSNInput();
-					launchManagerScreen(event);
+					System.out.println("-----INFO: Launching MgrScreen ----");
+					//launchManagerScreen(event);
+					result = getSSNInput();
+					System.out.println(result);
+					if(result == false) {
+						System.exit(1);
+					}
+					root = FXMLLoader.load(getClass().getClassLoader().getResource("ManagerScreen.fxml"));
+					System.out.println(root);
+					Stage stage = new Stage();
+					stage.setTitle("Manager Screen");
+					stage.setScene(new Scene(root, 800, 600));
+					stage.show();
 				}
-				catch(IOException e) {
-					System.out.println(e);
+				catch(Exception e) {
+					e.printStackTrace();
 				}
-				
 			}
 		});
 	}
 	
 	@FXML
-	private void getSSNInput() throws IOException {
-		//db_output.appendText("Java is shit");
-		//db_output.setText("Python is a better language");
+	private boolean getSSNInput() {
 		ssn = ssn_input.getText();
 		try {
-			CompanyDB company = new CompanyDB("@apollo.vse.gmu.edu:1521:ite10g","idiaz3","oahiwh");
-			mgrCheck = company.isManager(ssn);
-			if (mgrCheck)
-				db_output.setText("Welcome to the Company Database");
-			else
-				System.out.println("Not a manager or SSN does not exist in DB");
-				//System.exit(1);
-				
-			///* Open a new window */
-			//launchManagerScreen(null);
-			
-
+			try {
+				CompanyDB company = new CompanyDB("@apollo.vse.gmu.edu:1521:ite10g","idiaz3","oahiwh");
+				mgrCheck = company.isManager(ssn);
+				if (mgrCheck)
+					db_output.setText("Welcome to the Company Database");
+				else
+					System.out.println("Not a manager or SSN does not exist in DB");
+					return false;
+				///* Open a new window */
+				//launchManagerScreen(null);
+			}
+			catch (SQLException se) {
+				db_output.setText("ERROR: Cannot connect to database");
+			}
 		}
-		catch (SQLException se) {
-			db_output.setText("ERROR: Cannot connect to database");
+		catch(IOException e) {
+			System.out.println(e);
 		}
+		return true;
 		
 	}
 	
-	private void launchManagerScreen(ActionEvent event) throws IOException {
-		root = FXMLLoader.load(getClass().getResource("/ManagerScreen.fxml"));
-		Stage stage = new Stage();
-		stage.setTitle("Manager Screen");
-		stage.setScene(new Scene(root, 800, 600));
-		stage.show();
-		((Node)(event.getSource())).getScene().getWindow().hide();
-		
-	}
-
 }
