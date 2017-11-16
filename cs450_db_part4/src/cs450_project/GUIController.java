a3abc2f02244c7a062427d88ac43e56ca39af7b6
Ivan Diaz <idiaz3@gmu.edu>
2017-11-16 01:04:39 -0500

    This will now launch ManagerScreen

--- cs450_db_part4/src/cs450_project/GUIController.java
+++ cs450_db_part4/src/cs450_project/GUIController.java
@@ -3,11 +3,12 @@
  */
 package cs450_project;
 import javafx.event.*;
-import javafx.fxml.FXML;
+import javafx.fxml.*;
 import javafx.scene.*;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.*;
 import javafx.scene.text.*;
+import javafx.stage.Stage;
 import java.io.IOException;
 import java.sql.*;
 
@@ -20,11 +21,31 @@ public class GUIController {
 	private TextArea db_output;
 	@FXML
 	private TextField ssn_input;
+	@FXML
+	Button login_btn;
 	private String ssn;
 	private boolean mgrCheck;
+	Parent root;
 	
 	@FXML
-	protected void getSSNInput() throws IOException {
+	protected void initialize() {
+		login_btn.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				try {
+					getSSNInput();
+					launchManagerScreen(event);
+				}
+				catch(IOException e) {
+					System.out.println(e);
+				}
+				
+			}
+		});
+	}
+	
+	@FXML
+	private void getSSNInput() throws IOException {
 		//db_output.appendText("Java is shit");
 		//db_output.setText("Python is a better language");
 		ssn = ssn_input.getText();
@@ -36,11 +57,25 @@ public class GUIController {
 			else
 				System.out.println("Not a manager or SSN does not exist in DB");
 				//System.exit(1);
+				
+			///* Open a new window */
+			//launchManagerScreen(null);
+			
+
 		}
 		catch (SQLException se) {
 			db_output.setText("ERROR: Cannot connect to database");
 		}
-
+		
+	}
+	
+	private void launchManagerScreen(ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("/ManagerScreen.fxml"));
+		Stage stage = new Stage();
+		stage.setTitle("Manager Screen");
+		stage.setScene(new Scene(root, 800, 600));
+		stage.show();
+		((Node)(event.getSource())).getScene().getWindow().hide();
 		
 	}
 
