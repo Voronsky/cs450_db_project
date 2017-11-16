460ac2ef8c1143c2e93e924fa7ae9b953ec95b27
Ivan Diaz <idiaz3@gmu.edu>
2017-11-16 01:04:48 -0500

    Idk what I did it's late

--- cs450_db_part4/src/cs450_project/Main.java
+++ cs450_db_part4/src/cs450_project/Main.java
@@ -3,15 +3,19 @@ package cs450_project;
 import javafx.application.Application;
 import javafx.scene.layout.*;
 import javafx.fxml.*;
+import javafx.event.*;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.*;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.*;
 import java.util.*;
 
 
 public class Main extends Application {
+	@FXML
+	Button login_btn;
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -27,6 +31,11 @@ public class Main extends Application {
 			primaryStage.setTitle("CS450 Company Database");
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			/*login_btn.setOnAction(new EventHandler<ActionEvent>(){
+				public void handle(ActionEvent event) {
+					;
+				}
+			});*/
 			
 		} catch(Exception e) {
 			e.printStackTrace();
