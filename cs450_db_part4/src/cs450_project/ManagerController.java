730b349dc2fea954df3ebea2cd813298cc3ace1b
Ivan Diaz <idiaz3@gmu.edu>
2017-11-16 22:31:04 -0500

    ComboBox added, projects now applied

--- cs450_db_part4/src/cs450_project/ManagerController.java
+++ cs450_db_part4/src/cs450_project/ManagerController.java
@@ -11,6 +11,7 @@ import javafx.scene.*;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.*;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.text.*;
 import javafx.stage.Stage;
 import javafx.collections.*;
@@ -28,10 +29,10 @@ public class ManagerController {
 	private ComboBox<String> projectsBox;
 	
 	@FXML
-	private TextField first_name;
+	private TextField fname_input;
 
 	@FXML
-	private TextField last_name;
+	private TextField lname_input;
 	
 	@FXML
 	private TextField address_input;
@@ -40,23 +41,47 @@ public class ManagerController {
 	private TextField pHours;
 	
 	@FXML
-	private TextField middle_initial;
+	private TextField minit_input;
 	
 	@FXML
 	private TextField ssn_field;
 	
 	@FXML
 	private TextField birth_date;
+	
+	@FXML
+	private TextField dept_num;
+	
+	@FXML
+	private TextField dep_first_name;
+	
+	@FXML
+	private TextField salary_input;
+	
+	@FXML
+	private TextArea output;
 
 	
 	@FXML
 	private Button add_emp_btn;
+	
+	@FXML
+	private Button assignProject;
 
 	CompanyDB company;
 	Employee employee;
+	Pair pair;
+	Pair projectPair;
+	private static final double MAX_HOURS = 40.00;
 	private ArrayList<String> listOfProjects;
-	private ArrayList<String> selectedProjects;
+	//private ArrayList<Pair<String, Double>> selectedProjects = new ArrayList<Pair<String, Double>>();
+	private ArrayList<Pair> selectedProjects = new ArrayList<Pair>();
+	private ArrayList<String> missingFields = new ArrayList<String>();
 	private ObservableList<String> projectList = FXCollections.observableArrayList();
+	private Alert alert = new Alert(AlertType.ERROR);
+	private double totalHours = 0;
+	private String missingValues = "";
+	private String outputText = "";
 	
 	
 	@FXML
@@ -65,6 +90,8 @@ public class ManagerController {
 		try {
 			
 			//project_name_col.setCellFactory(new PropertyValueFactory<Project, String>("Test"));
+			alert.setTitle("Employee Creation Error");
+			alert.setHeaderText("Attention Required!");
 			company = new CompanyDB("@apollo.vse.gmu.edu:1521:ite10g","idiaz3","oahiwh");
 			listOfProjects = company.getProjectList();
 			System.out.println("---List of Projects --- ");
@@ -93,13 +120,85 @@ public class ManagerController {
 			}
 		});
 		
+		assignProject.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				pair = new Pair();
+				totalHours = 0.00;
+				pair.setHours(Double.parseDouble(pHours.getText()));
+				pair.setProject(projectsBox.getSelectionModel().getSelectedItem().toString());
+				System.out.println("---- INFO: Project Selected and Hours assigned ---");
+				System.out.println(pair.getProject()+"\t"+pair.getHours());
+				selectedProjects.add(pair);
+				for(int i=0; i<selectedProjects.size(); i++) {
+					projectPair = selectedProjects.get(i);
+					totalHours += projectPair.getHours();
+					if(totalHours > MAX_HOURS) {
+						selectedProjects.clear();
+						outputText = "";
+						alert.setHeaderText("More than max Hours allowed");
+						alert.setContentText("No more than a total of 40 hours can be assigned to an employee. Try again");
+						alert.showAndWait();
+					}
+				}
+				outputText = outputText + "\n" + "Assigned " + pair.getProject() + " to Employee\t Hours: " + pair.getHours();
+				output.setText(outputText);
+
+			}
+		});
+		
+	
 		
 	}
 	
 	@FXML
 	protected void insertIntoDB() {
-		employee = new Employee(first_name.getText(),middle_initial.getText(),last_name.getText(),address_input.getText(),
-				ssn_field.getText(),birth_date.getText(),0,0);
+		missingValues = "";
+		try {
+			if(fname_input.getText().isEmpty()) {
+				missingValues = missingValues + "\nMissing First Name";
+			}
+			if(minit_input.getText().isEmpty()) {
+				missingValues = missingValues + "\nMissing Middle Initial";
+			}
+			if(lname_input.getText().isEmpty()) {
+				missingValues = missingValues + "\nMissing Last Name";
+			}
+			if(address_input.getText().isEmpty()) {
+				missingValues = missingValues + "\nMissing Address";
+			}
+			if(ssn_field.getText().isEmpty()) {
+				missingValues = missingValues + "\nMissing SSN";
+			}
+			if(birth_date.getText().isEmpty()) {
+				missingValues = missingValues + "\nMissing Birth date";
+			}
+			if(salary_input.getText().isEmpty()) {
+				missingValues = missingValues + "\nMissing Salary";
+			}
+			if(pHours.getText().isEmpty()) {
+				missingValues = missingValues + "\nMissing Number of Hours for Project";
+			}
+			
+			if(dept_num.getText().isEmpty()) {
+				missingValues = missingValues + "\nMissing Department Values";
+			}
+			
+			if(missingValues.length()>0) {
+				alert.setContentText(missingValues);
+				alert.showAndWait();
+			}
+			else {
+				employee = new Employee(fname_input.getText(),minit_input.getText(),lname_input.getText(),
+				address_input.getText(), ssn_field.getText(),birth_date.getText(),
+				Integer.parseInt(salary_input.getText()),Integer.parseInt(dept_num.getText()));
+				employee.setAssignedProjects(selectedProjects);
+				System.out.println(employee);
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 
