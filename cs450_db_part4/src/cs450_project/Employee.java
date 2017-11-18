966d90253b5eeb4704cd91272a3619353e160fc6
Ivan Diaz <idiaz3@gmu.edu>
2017-11-17 19:48:11 -0500

    New inner class, Dependent, for Dependents

--- cs450_db_part4/src/cs450_project/Employee.java
+++ cs450_db_part4/src/cs450_project/Employee.java
@@ -17,19 +17,25 @@ public class Employee {
 	private String address;
 	private String birthDate;
 	private String ssn;
-	private String mgrSsn;
+	private String superSsn;
 	private int salary;
 	private int dno;
 	private ArrayList<Pair> assignedProjects = new ArrayList<Pair>();
+	private ArrayList<Dependent> deps = new ArrayList<Dependent>();
 
 
-	public Employee(String firstName, String minit, String lastName, String sex, String Address, String ssn, String mgrSsn, String birthDate,
+	public Employee() {
+		
+		
+	}
+	public Employee(String firstName, String minit, String lastName, String sex, 
+			String Address, String ssn, String superSsn, String birthDate,
 			int salary, int dno) {
 		this.fname = firstName;
 		this.minit = minit;
 		this.lname = lastName;
 		this.sex = sex;
-		this.mgrSsn = mgrSsn;
+		this.superSsn = superSsn;
 		this.address = Address;
 		this.ssn = ssn;
 		this.birthDate = birthDate;
@@ -70,8 +76,8 @@ public class Employee {
 		return this.sex;
 	}
 	
-	public String getManagerSSN() {
-		return this.mgrSsn;
+	public String getSupervisorSSN() {
+		return this.superSsn;
 	}
 	
 	public int getSalary() {
@@ -86,13 +92,59 @@ public class Employee {
 		return this.assignedProjects;
 	}
 	
+	public ArrayList<Dependent> getDepedent() {
+		return this.deps;
+	}
+	
+	public void setFirstName(String fname) {
+		this.fname = fname;
+	}
+	
+	public void setMiddleInitial(String minit) {
+		this.minit = minit;
+	}
+	
+	public void setLastName(String lname) {
+		this.lname = lname;
+	}
+	
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public void setBirthDate(String bdate) {
+		this.birthDate = bdate;
+	}
+	
+	public void setSSN(String ssn) {
+		this.ssn = ssn;
+	}
+	
+	public void setSupervisorSSN(String superssn) {
+		this.superSsn = superssn;
+	}
+	
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	public void setDepartmentNumber(int dno) {
+		this.dno = dno;
+	}
+
 	public void setAssignedProjects(ArrayList<Pair> listOfProjects) {
 		this.assignedProjects = listOfProjects;
 		
 	}
 	
-	public void setManagerSSN(String mgrssn) {
-		this.mgrSsn = mgrssn;
+
+	public void setDependents(String fname, String sex, String bdate, String relationship) {
+		Dependent dep = new Dependent(fname,sex,bdate,relationship);
+		deps.add(dep);
 	}
 	
 	/**
@@ -101,21 +153,66 @@ public class Employee {
 	public void printDebugEmployeeeInfo() {
 		ArrayList<Pair> debug = new ArrayList<Pair>();
 		Pair debugPair = new Pair();
+		Dependent debugDep = new Dependent();
 		System.out.println("---EMPLOYEE OBJECT " + this + " -----");
 		System.out.println("Employee Name: "+this.getEmployeeName());
 		System.out.println("Sex: "+this.getSex());
 		System.out.println("Address: "+this.getAddress());
 		System.out.println("Employee SSN: "+this.getSSN());
-		System.out.println("Manager SSN: "+this.getManagerSSN());
+		System.out.println("Supervisor SSN: "+this.getSupervisorSSN());
 		System.out.println("Employee Birth date: "+this.getBirthDate());
 		System.out.println("Employee Salary: "+this.getSalary());
 		System.out.println("Employee Department Number: "+this.getDepartmentNum());
+		System.out.println("Assigned Projects:");
 		debug = this.getAssignedProjects();
 		for(int i = 0 ; i < debug.size(); i++) {
 			debugPair = debug.get(i);
 			System.out.println(debugPair.getProject()+"\t"+debugPair.getHours());
 		}
+		System.out.println("---DEPENDENTS----");
+		for(int i=0; i<this.deps.size(); i++) {
+			debugDep = deps.get(i);
+			System.out.println(debugDep.getName()+" "+debugDep.getSex()+" "
+			+debugDep.getBirthDate()+" "+debugDep.getRelationship());
+		}
 		System.out.println("----END OF EMPLOYEE " + this + "DEBUG ------");
+		
+	}
+	
+	private class Dependent {
+		private String name;
+		private String sex;
+		private String bdate;
+		private String relationship;
+		
+		private Dependent() {
+			
+		}
+		
+		private Dependent( String name, String sex, 
+				String bdate, String relationship) 
+		{
+			this.name = name;
+			this.sex = sex;
+			this.bdate = bdate;
+			this.relationship = relationship;
+		}
+		
+		private String getName() {
+			return this.name;
+		}
+		
+		private String getSex() {
+			return this.sex;
+		}
+		
+		private String getBirthDate() {
+			return this.bdate;
+		}
+		
+		private String getRelationship() {
+			return this.relationship;
+		}
 		
 	}
 	
