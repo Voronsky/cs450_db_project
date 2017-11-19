264aa14c5f2eb730c99a198b1ba4fc520c929407
Ivan Diaz <idiaz3@gmu.edu>
2017-11-19 12:41:06 -0500

    implemented employee insert,JDBC is buggy or maybe it's me.

--- cs450_db_part4/src/cs450_project/CompanyDB.java
+++ cs450_db_part4/src/cs450_project/CompanyDB.java
@@ -3,14 +3,26 @@
  */
 package cs450_project;
 import java.sql.*;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.io.*;
 import java.util.*;
+import java.math.BigDecimal;
 
 /**
  * @author ivan
- *
+ * This class handles formatting the Employee Class field members to be in 
+ * Oracle Data types. Theres a few conversions that had to be done as there is no
+ * natural 1:1 from Java data types to Oracle.
+ * Thus NUMBER in Oracle could only be accepted from BigDecimal data types, thus
+ * integers had to be converted into BigDecimal. Same applies to DATE
+ * Oracle DATE had to be a conversion from a formated Java DATE datatype
+ * Oracle Char had to be passed as a value from String.ValueOf as there is no
+ * method for JDBC called 'setChar()'.
  */
+
 public class CompanyDB {
+	SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yy");
 	private String db_url = "";
 	private String pass = "";
 	private String username="";
@@ -85,10 +97,48 @@ public class CompanyDB {
 		return listOfProjects;
 
 	}
+	
+	public BigDecimal getProjectNumber(String pName) throws SQLException {
+		String query = "select pnumber from project where pname = ?";
+		BigDecimal pno = null;
+		System.out.print("Project Name to get PNO from:" + pName);
+		p = conn.prepareStatement(query);
+		p.clearParameters();
+		p.setString(1, pName);
+		r = p.executeQuery();
+		while(r.next()) {
+			pno = r.getBigDecimal(1);
+		}
+		return pno;
+		
+	}
 
+	/**
+	 * This prepares all the Employee field members into appropriate oracle Datatypes
+	 * if necessary. Then inserts the contents of the employee object into the DB
+	 * @param e - of Employee type
+	 * @throws SQLException
+	 */
 	public void insertIntoEmployeeTable(Employee e) throws SQLException {
-		String query = "insert INTO (fname,minit,lname,ssn,bdate,address,sex,salary,superssn,dno)"
-				+ " VALUES (?,?,?,?,?,?,?,?,?,?)";
+		System.out.println("ENTER EMPLOYEE TABLE INSERT");
+		e.printDebugEmployeeeInfo();
+		BigDecimal numberSalary = new BigDecimal(e.getSalary());
+		BigDecimal numberDeptNum = new BigDecimal(e.getDepartmentNum());
+		java.sql.Date sqlDate = null;
+		try {
+			java.util.Date javaDate = sdf.parse(e.getBirthDate());
+			System.out.println("javaDate: "+javaDate);
+			sqlDate = new java.sql.Date(javaDate.getTime());
+		}catch(Exception z) {
+			z.printStackTrace();
+		}
+
+		String query = "INSERT INTO EMPLOYEE (fname,minit,lname,ssn,bdate,address,sex,salary,superssn,dno,email)"
+				+ "VALUES(?,?,?,?,?,?,?,?,?,?,null)";
+		
+		/**
+		 * varchar,varchar,vchar,char,date,varchar,char,number,char,number,varchar
+		 */
 
 		p = conn.prepareStatement(query);
 		p.clearParameters();
@@ -96,14 +146,96 @@ public class CompanyDB {
 		p.setString(2, e.getMinit());
 		p.setString(3, e.getLastName());
 		p.setString(4, e.getSSN());
-		p.setString(5, e.getBirthDate());
+		//p.setDate(5, sqlDate); //birthdate
+		//p.setDate(5, (Date)"27-JAN-93");
+		p.setDate(5, null);
 		p.setString(6, e.getAddress());
 		p.setString(7, e.getSex());
-		p.setString(8, Integer.toString(e.getSalary()));
+		p.setBigDecimal(8, numberSalary);
 		p.setString(9, e.getSupervisorSSN());
-		p.setString(10, Integer.toString(e.getDepartmentNum()));
-		r = p.executeQuery();
+		p.setBigDecimal(10, numberDeptNum);
+		//p.executeQuery();
+		p.executeUpdate();
 		System.out.println(r);
+		System.out.println("---CHECKING VALUES---");
+		//System.out.println(sqlDate);
+		System.out.println("EXITED EMPLOYEE TABLE INSERT");
 		
+	}
+	
+	/**
+	 * Formats project values of employee to be put into the works_on table
+	 * The Oracle types are (Char, Number, Number)
+	 * It will grab each project name and hours for that project from listOfProjects
+	 * then grab each pno associated to that project 
+	 * and then it will put into an object type of ProjectPair(Pno,Hours).
+	 * From here it will make a list of ProjectPairs, and then for each pair
+	 * insert individually into the table
+	 * @param e
+	 * @throws SQLException
+	 */
+	public void insertIntoWorksOn(Employee e) throws SQLException {
+		System.out.println("ENTERED INSERTWORKS ON TABLE");
+		ArrayList<ProjectPair> numOfProjects = new ArrayList<ProjectPair>();
+		String pName = null;
+		BigDecimal pno;
+		BigDecimal hours;
+		//String pnoQuery = "select pno from project where pname = ?";
+		String insertQuery = "insert INTO works_on(essn,pno,hours) values(?,?,?)";
+		for(int i = 0; i<e.getAssignedProjects().size();i++) {
+			pName = e.getProjectName(i);
+			pno = getProjectNumber(pName);
+			hours = new BigDecimal(e.getProjectHours(i));
+			ProjectPair pair = new ProjectPair(pno, hours);
+			pair.debugPrint();
+			numOfProjects.add(pair);
+			/*p = conn.prepareStatement(pnoQuery);
+			p.clearParameters();
+			p.setString(1, pName);
+			r = p.executeQuery(pnoQuery);
+			while(r.next()) {
+				pno = r.getBigDecimal(1);
+				hours = new BigDecimal(e.getProjectHours(i));
+				ProjectPair pair = new ProjectPair(pno, hours);
+				pair.debugPrint();
+				numOfProjects.add(pair);
+			}*/
+		}
+		
+		for(int i = 0; i<numOfProjects.size();i++) {
+			p = conn.prepareStatement(insertQuery);
+			p.clearParameters();
+			p.setString(1,e.getSSN());
+			p.setBigDecimal(2, numOfProjects.get(i).getPno());
+			p.setBigDecimal(3, numOfProjects.get(i).getHours());
+			p.executeUpdate();
+		}
+	}
+	
+	
+	
+	private class ProjectPair  {
+		private BigDecimal pno;
+		private BigDecimal hours;
+		
+		private ProjectPair(BigDecimal pno, BigDecimal hours) {
+			this.pno = pno;
+			this.hours = hours;
+		}
+		
+		private BigDecimal getPno() {
+			return this.pno;
+		}
+		
+		private BigDecimal getHours() {
+			return this.hours;
+		}
+		
+		private void debugPrint() {
+			System.out.println("---DEBUG PROJECTPAIR ---");
+			System.out.println(this.pno);
+			System.out.println(this.hours);
+			System.out.println("----END OF DEBUG PROJECTPAIR --");
+		}
 	}
 }
