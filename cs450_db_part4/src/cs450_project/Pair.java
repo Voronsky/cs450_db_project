03d6fc4e05360d82f684522ac5bdd393bc27b65a
Ivan Diaz <idiaz3@gmu.edu>
2017-11-22 11:56:47 -0500

    Added Pno field to be BigDecimal for SQL reasons

--- cs450_db_part4/src/cs450_project/Pair.java
+++ cs450_db_part4/src/cs450_project/Pair.java
@@ -3,6 +3,8 @@
  */
 package cs450_project;
 
+import java.math.BigDecimal;
+
 /**
  * @author ivan
  * A class of Pairs, this is a custom class where each pair specifically is used in manner
@@ -10,6 +12,7 @@ package cs450_project;
  */
 public class Pair {
 		private String project;
+		private BigDecimal pno;
 		private double hours;
 		
 		public Pair() {
@@ -25,12 +28,20 @@ public class Pair {
 			return this.project;
 		}
 		
+		public BigDecimal getProjectPno() {
+			return this.pno;
+		}
+		
 		public double getHours() {
 			return this.hours;
 		}
 		
 		public void setProject(String p) {
 			this.project = p;
+		}
+		
+		public void setProjectPno(BigDecimal p) {
+			this.pno = p;
 		}
 		
 		public void setHours(double parseDouble ) {
